16b583c5217ad7b3a503199daf54153d889144ff
HeidkeRed <cebg_slp@hotmail.com>
2024-11-01 18:35:39 -0600

    xd2

--- formulario_s/settings.py
+++ formulario_s/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'railway',
         'USER': 'root',
         'PASSWORD': 'WjgGSEMGBspDhvRjBnQPaETSkARROpdi',
-        'HOST': 'mysql://root:WjgGSEMGBspDhvRjBnQPaETSkARROpdi@autorack.proxy.rlwy.net:15567/railway',
+        'HOST': 'autorack.proxy.rlwy.net',
         'PORT': '3306',
     }
 }
